44708a891d41f3726ba5e2854505088ec0dc17a5
Julius Park <juliusgeo@gmail.com>
2022-12-06 17:31:22 -0800

    saving refactor, add raytracing example

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from math import sqrt, cos, sin
-from uuid import UUID
-
+from uuid import uuid4
+from enum import Enum
+from functools import cached_property, cache
 EPSILON = .0001
 
 
+def clamper(x, l, u=None):
+    return l if x < l else u if x > u else x
+
 class Tuple:
-    __slots__ = ("x", "y", "z", "w", "m")
-    dims = ("x", "y", "z")
+    __slots__ = ("x", "y", "z", "w", "m", "_hash")
+    dims = ("x", "y", "z", "w")
 
     def __init__(self, x, y=0, z=0, w=0):
         self.x = x
@@ -14,51 +18,78 @@ class Tuple:
         self.z = z
         self.w = w
         self.m = sqrt(self.x**2+self.y**2+self.z**2+self.w**2)
+        self._hash = self.hash()
+
+    def hash(self):
+        return sum([hash(i) for i in self])
+
+    def __hash__(self):
+        return self._hash
 
     def __eq__(self, other):
-        for i in self.dims:
-            if abs(getattr(self, i) - getattr(other, i)) > EPSILON:
-                return False
-        return True
+        if isinstance(other, tuple):
+            m_i_n = min(len(self.dims), len(other))
+            return all([abs(getattr(self, l) - r) < EPSILON for l, r in zip(self.dims[:m_i_n], other[:m_i_n])])
+        return all([abs(i-j) < EPSILON for i, j in zip(self, other)])
 
+    @cache
     def __add__(self, other):
-        return self.type_from_op(other)(*[getattr(self, i)+getattr(other, i) for i in self.dims])
+        if not isinstance(other, Tuple):
+            return type(self)(*[i+other for i in self])
+        return self.type_from_op(other)(*[i+j for i, j in zip(self, other)])
 
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    @cache
     def __sub__(self, other):
-        return self.type_from_op(other)(*[getattr(self, i) - getattr(other, i) for i in self.dims])
+        if not isinstance(other, Tuple):
+            return type(self)(*[i-other for i in self])
+        return self.type_from_op(other)(*[i - j for i, j in zip(self, other)])
 
+    def __rsub__(self, other):
+        return self.__sub__(other)
+
+    @cache
     def __neg__(self):
         return Tuple(-self.x, -self.y, -self.z, -self.w)
 
+    @cache
     def __mul__(self, other):
         if isinstance(other, Matrix):
             return Matrix.__mul__(other, self)
         elif not isinstance(other, Vector):
-            return type(self)(*[getattr(self, i)*other for i in self.dims])
+            return type(self)(*[i*other for i in self])
         else:
             return self.cross(other)
 
-    def __truediv__(self, other):
+    @cache
+    def __truediv__(self, other, inverse=False):
         if not isinstance(other, Tuple):
-            return type(self)(*[getattr(self, i) / other for i in self.dims])
+            return type(self)(*[i / other for i in self])
         else:
             raise TypeError
 
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
     def __repr__(self):
-        return f"{type(self).__name__}({self.x}, {self.y}, {self.z})"
+        return f"{type(self).__name__}({self.x}, {self.y}, {self.z}, {self.w})"
 
     def __abs__(self):
         return self.m
 
     def __iter__(self):
-        for i in [self.x, self.y, self.z, self.w]:
-            yield i
+        return iter([self.x, self.y, self.z, self.w])
 
+    @cache
     def normalize(self):
-        return type(self)(self.x/abs(self), self.y/abs(self), self.z/abs(self), self.w/abs(self))
+        return type(self)(self.x/self.m, self.y/self.m, self.z/self.m, self.w/self.m)
 
+    @cache
     def dot(self, other):
-        return sum([getattr(self, i) * getattr(other, i) for i in self.dims+tuple("w")])
+        return sum([i*j for i, j in zip(self, other)])
 
     def type_from_op(self, other):
         if type(self) == Color:
@@ -71,19 +102,30 @@ class Tuple:
             return Vector
         else:
             return Tuple
-
+    @cache
+    def invert(self):
+        return type(self)(*[1/i for i in self if i != 0])
 
 class Point(Tuple):
     w = 1
+    dims = ("x", "y", "z")
+
+    def __init__(self, x, y, z, w=1):
+        super().__init__(x, y, z, 1)
 
 
 class Vector(Tuple):
     w = 0
+    dims = ("x", "y", "z")
 
+    def __init__(self, x, y, z, w=0):
+        super().__init__(x, y, z, 0)
+
+    @cache
     def cross(self, other):
         return Vector(self.y * other.z - self.z * other.y,
-                self.z * other.x - self.x * other.z,
-                self.x * other.y - self.y * other.x)
+                      self.z * other.x - self.x * other.z,
+                      self.x * other.y - self.y * other.x)
 
 
 class Projectile:
@@ -108,13 +150,11 @@ class Color(Tuple):
 
     def __mul__(self, other):
         if not isinstance(other, Color):
-            return Color(*[getattr(self, i)*other for i in self.dims])
+            return Color(*[i*other for i in self])
         return Color(self.x*other.x, self.y*other.y, self.z*other.z)
 
     def clamp(self):
-        def clamper(x, l, u):
-            return l if x < l else u if x > u else x
-        return Color(*[round(clamper(getattr(self, i), 0, 255)) for i in self.dims])
+        return Color(*[round(clamper(i, 0, 255)) for i in self])
 
     def __str__(self):
         clamped = (self*255).clamp()
@@ -173,13 +213,32 @@ class Canvas:
         else:
             return self.ppm_header()+self.ppm_body()
 
+class TransformType(Enum):
+    SHEARING = "shearing"
+    TRANSLATING = "translating"
+    SCALING = "scaling"
+    ROTATING = "rotating"
+    IDENTITY = "identity"
+    UNKNOWN = None
 
 class Matrix:
-    def __init__(self, mat):
+    def __init__(self, mat, transform_type=TransformType.UNKNOWN):
         self.mat = mat
+        self.transform_type = transform_type
+        self.size = (len(mat), len(mat[0]))
+        self.hash = self.hash()
+
+    def hash(self):
+        h = 0
+        for row in self.mat:
+            h += hash(tuple(row))
+        return h + hash(self.transform_type)
+
+    def __hash__(self):
+        return self.hash
 
     def __eq__(self, other):
-        return all([abs(i-n) < EPSILON for i, n in zip(self, other)])
+        return all([abs(i-n) < EPSILON for l, r in zip(self.mat, other.mat) for i, n in zip(l, r)])
 
     def __getitem__(self, item):
         return self.mat[item]
@@ -196,23 +255,26 @@ class Matrix:
         return out
 
     def __iter__(self):
-        return iter([i for row in self.mat for i in row])
+        return iter(self.mat)
 
     def pop(self, idx):
         return Matrix([sublist for i, sublist in enumerate(self.mat) if i != idx])
 
+    @cache
     def __mul__(self, other):
         if isinstance(other, Tuple):
-            return Tuple(*[Tuple(*row).dot(other) for row in self.mat])
+            return type(other)(*[sum([i*n for i, n in zip(row, other)]) for row in self.mat])
         if isinstance(other, Matrix):
-            return Matrix([[Tuple(*row).dot(Tuple(*col)) for col in other.transpose().mat] for row in self.mat])
+            return Matrix([[sum([i*n for i, n in zip(row, col)]) for col in other.transpose().mat] for row in self.mat])
         return Matrix([[i*other for i in row] for row in self.mat])
 
+    @cache
     def transpose(self):
         return Matrix(list(zip(*self.mat)))
 
+    @cache
     def determinant(self):
-        if len(self.mat) > 2:
+        if self.size[0] > 2:
             return sum([self.mat[0][i]*self.cofactor(0, i) for i in range(len(self.mat[0]))])
         return Matrix.determ_base(Matrix(self.mat))
 
@@ -220,31 +282,45 @@ class Matrix:
     def determ_base(mat):
         return (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0])
 
+    @cache
     def submatrix(self, row, column):
         return Matrix(self.pop(row).transpose().pop(column).transpose().mat)
 
+    @cache
     def minor(self, row, column):
         return self.submatrix(row, column).determinant()
 
+    @cache
     def cofactor(self, row, column):
         return self.minor(row, column)*(1 if (row+column) % 2 == 0 else -1)
 
+    @cache
     def invertible(self):
         return not (self.determinant() == 0)
 
+    @cached_property
+    def inv(self):
+        return self.inverse()
+
     def inverse(self):
         if not self.invertible():
-            raise TypeError
+            return None
         det = self.determinant()
         return Matrix([[self.cofactor(i, n)/det for i in range(len(self.mat))] for n in range(len(self.mat[0]))])
 
     @staticmethod
     def translating(x, y, z):
-        return Matrix([[1, 0, 0, x], [0, 1, 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
+        return Matrix([[1, 0, 0, x],
+                       [0, 1, 0, y],
+                       [0, 0, 1, z],
+                       [0, 0, 0, 1]], transform_type=TransformType.TRANSLATING)
 
     @staticmethod
     def scaling(x, y, z):
-        return Matrix([[x, 0, 0, 0], [0, y, 0, 0], [0, 0, z, 0], [0, 0, 0, 1]])
+        return Matrix([[x, 0, 0, 0],
+                       [0, y, 0, 0],
+                       [0, 0, z, 0],
+                       [0, 0, 0, 1]], transform_type=TransformType.SCALING)
 
     @staticmethod
     def rotating(r, axis=0):
@@ -252,17 +328,17 @@ class Matrix:
             return Matrix([[1,      0,       0, 0],
                            [0, cos(r), -sin(r), 0],
                            [0, sin(r),  cos(r), 0],
-                           [0,      0,       0, 1]])
+                           [0,      0,       0, 1]], transform_type=TransformType.ROTATING)
         elif axis == 1:
             return Matrix([[cos(r),  0, sin(r), 0],
                            [0,       1,      0, 0],
                            [-sin(r), 0, cos(r), 0],
-                           [0,       0,      0, 1]])
+                           [0,       0,      0, 1]], transform_type=TransformType.ROTATING)
         elif axis == 2:
             return Matrix([[cos(r), -sin(r), 0, 0],
                            [sin(r),  cos(r), 0, 0],
                            [0,            0, 1, 0],
-                           [0,            0, 0, 1]])
+                           [0,            0, 0, 1]], transform_type=TransformType.ROTATING)
         raise TypeError("Invalid axis!")
 
     @staticmethod
@@ -270,7 +346,15 @@ class Matrix:
         return Matrix([[1,  xy, xz, 0],
                        [yx,  1, yz, 0],
                        [zx, zy,  1, 0],
-                       [0,   0,  0, 1]])
+                       [0,   0,  0, 1]], transform_type=TransformType.SHEARING)
+
+    @staticmethod
+    def eye():
+        return Matrix([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0],
+                       [0, 0, 0, 1]], transform_type=TransformType.IDENTITY)
+
 
 class Ray:
     def __init__(self, origin, direction):
@@ -280,13 +364,80 @@ class Ray:
     def position(self, d):
         return self.origin + self.direction*d
 
+    def transform(self, m):
+        return Ray(self.origin*m, self.direction*m)
+
+    def __repr__(self):
+        return f"origin: {self.origin}, direction: {self.direction}"
+
 
 class Sphere:
-    def __init__(self, radius):
-        self.id = UUID()
+    def __init__(self, radius, transform=Matrix.eye()):
+        self.id = uuid4()
         self.radius = radius
+        self.transform = transform
+        self._hash = self.hash()
 
-    def intersection(self, ray):
+    def hash(self):
+        return sum([hash(self.id), hash(self.radius), hash(self.transform)])
+
+    def __hash__(self):
+        return self._hash
+
+    @cache
+    def __and__(self, other):
+        if not isinstance(other, Ray):
+            return None
+        other = other.transform(self.transform.inverse())
+        sphere_to_ray = other.origin - Point(0, 0, 0)
+        a = other.direction.dot(other.direction)
+        b = 2*other.direction.dot(sphere_to_ray)
+        c = sphere_to_ray.dot(sphere_to_ray) - 1
+        disc = b**2 - (4 * a * c)
+        two_a = 2*a
+        return None if disc < 0 else ((-b-sqrt(disc))/two_a, (-b+sqrt(disc))/two_a)
+
+    def __rand__(self, other):
+        return self.__and__(other)
 
 
+class Intersection:
+    def __init__(self, t, object):
+        self.obj = object
+        self.t = t
 
+    def __add__(self, other):
+        if isinstance(other, list):
+            return other.append(self)
+        return [self, other]
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    @staticmethod
+    def hit(l):
+        return min([i for i in l if i is not None and i >= 0], default=None)
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.t == other
+        else:
+            return self.t == other.t and self.obj == other.obj
+
+    def __le__(self, other):
+        if isinstance(other, int):
+            return self.t <= other
+        else:
+            return self.t <= other.t
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, int):
+            return self.t < other
+        else:
+            return self.t < other.t
+
+    def __gt__(self, other):
+        return not self.__le__(other) and not self.__eq__(other)
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-from main import Point, Vector, Tuple, Projectile, Environment, Color, Canvas, Matrix, Ray
+import os
+
+from main import Point, Vector, Tuple, Projectile, Environment, Color, Canvas, Matrix, Ray, Sphere, Intersection
 
 import unittest
 import math
@@ -59,7 +61,8 @@ class TupleTests(unittest.TestCase):
 
     def test_normalize(self):
         self.assertEqual(Vector(4, 0, 0).normalize(), Vector(1, 0, 0))
-        self.assertEqual(Vector(1, 2, 3).normalize(), Vector(0.26726, 0.53452, 0.80178))
+        self.assertEqual(Vector(1, 2, 3).normalize(),
+                         Vector(0.26726, 0.53452, 0.80178))
 
     @unittest.skip
     def test_projectiles(self):
@@ -70,10 +73,13 @@ class TupleTests(unittest.TestCase):
             e.tick(p)
 
     def test_colors(self):
-        self.assertEqual(Color(0.9, 0.6, 0.75) + Color(0.7, 0.1, 0.25), Color(1.6, 0.7, 1.0))
-        self.assertEqual(Color(0.9, 0.6, 0.75) - Color(0.7, 0.1, 0.25), Color(0.2, 0.5, 0.5))
+        self.assertEqual(Color(0.9, 0.6, 0.75) +
+                         Color(0.7, 0.1, 0.25), Color(1.6, 0.7, 1.0))
+        self.assertEqual(Color(0.9, 0.6, 0.75) -
+                         Color(0.7, 0.1, 0.25), Color(0.2, 0.5, 0.5))
         self.assertEqual(Color(0.2, 0.3, 0.4) * 2, Color(0.4, 0.6, 0.8))
-        self.assertEqual(Color(1, 0.2, 0.4) * Color(0.9, 1, 0.1), Color(0.9, 0.2, 0.04))
+        self.assertEqual(Color(1, 0.2, 0.4) * Color(0.9,
+                         1, 0.1), Color(0.9, 0.2, 0.04))
 
     def test_canvas(self):
         c = Canvas(10, 20)
@@ -112,7 +118,8 @@ class TupleTests(unittest.TestCase):
         e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0))
         c = Canvas(900, 550)
         while p.position.y > 0:
-            c.write_pixel(round(p.position.x), c.height - round(p.position.y), Color(1, 1, 1))
+            c.write_pixel(round(p.position.x), c.height -
+                          round(p.position.y), Color(1, 1, 1))
             e.tick(p)
         c.ppm_file("out.ppm")
 
@@ -126,21 +133,22 @@ class TupleTests(unittest.TestCase):
                      [4, 3, 6, 5],
                      [1, 2, 7, 8]])
 
-        self.assertEqual(m1*m2, Matrix([[20, 22, 50, 48], [44, 54, 114, 108], [40, 58, 110, 102], [16, 26, 46, 42]]))
+        self.assertEqual(
+            m1*m2, Matrix([[20, 22, 50, 48], [44, 54, 114, 108], [40, 58, 110, 102], [16, 26, 46, 42]]))
 
     def test_mat_tuple_mult(self):
         m = Matrix([[1, 2, 3, 4],
-        [2, 4, 4, 2],
-        [8, 6, 4, 1],
-        [0, 0, 0, 1]])
+                    [2, 4, 4, 2],
+                    [8, 6, 4, 1],
+                    [0, 0, 0, 1]])
         b = Tuple(1, 2, 3, 1)
         self.assertEqual(m*b, Tuple(18, 24, 33, 1))
 
     def test_mat_transpose(self):
         self.assertEqual(Matrix([[0, 9, 3, 0],
-        [9, 8, 0, 8],
-        [1, 8, 5, 3],
-        [0, 0, 5, 8]]).transpose(), Matrix([[0, 9, 1, 0], [9, 8, 8, 0], [3, 0, 5, 5], [0, 8, 3, 8]]))
+                                 [9, 8, 0, 8],
+                                 [1, 8, 5, 3],
+                                 [0, 0, 5, 8]]).transpose(), Matrix([[0, 9, 1, 0], [9, 8, 8, 0], [3, 0, 5, 5], [0, 8, 3, 8]]))
 
     def test_mat_minor(self):
         m = Matrix([[3, 5, 0], [2, -1, -7], [6, -1, 5]])
@@ -158,28 +166,33 @@ class TupleTests(unittest.TestCase):
                          Matrix([[-6, 1, 6], [-8, 8, 6], [-7, -1, 1]]))
 
     def test_larger_mat_determinant(self):
-        self.assertEqual(Matrix([[1, 2, 6], [-5, 8, -4], [2, 6, 4]]).determinant(), -196)
-        m = Matrix([[-2, -8, 3, 5], [-3, 1, 7, 3], [1, 2, -9, 6], [-6, 7, 7, -9]])
+        self.assertEqual(
+            Matrix([[1, 2, 6], [-5, 8, -4], [2, 6, 4]]).determinant(), -196)
+        m = Matrix([[-2, -8, 3, 5], [-3, 1, 7, 3],
+                   [1, 2, -9, 6], [-6, 7, 7, -9]])
         self.assertEqual(m.determinant(), -4071)
         self.assertEqual(m.cofactor(0, 1), 447)
-    
+
     def test_inversion(self):
         m = Matrix([[8, -5, 9, 2],
                     [7, 5, 6, 1],
                     [-6, 0, 9, 6],
                     [-3, 0, -9, -4]])
 
-        self.assertEqual(m.inverse(), Matrix([[-0.15385, -0.15385, -0.28205, -0.53846],
-                                              [-0.07692, 0.12308, 0.02564, 0.03077],
+        self.assertEqual(m.inv, Matrix([[-0.15385, -0.15385, -0.28205, -0.53846],
+                                              [-0.07692, 0.12308,
+                                                  0.02564, 0.03077],
                                               [0.35897, 0.35897, 0.43590, 0.92308],
                                               [-0.69231, -0.69231, -0.76923, -1.92308]]))
         m = Matrix([[9, 3, 0, 9],
-             [-5, -2, -6, -3],
-             [-4, 9, 6, 4],
-             [-7, 6, 6, 2]])
+                    [-5, -2, -6, -3],
+                    [-4, 9, 6, 4],
+                    [-7, 6, 6, 2]])
         self.assertEqual(m.inverse(), Matrix([[-0.04074, -0.07778, 0.14444, -0.22222],
-                                              [-0.07778, 0.03333, 0.36667, -0.33333],
-                                              [-0.02901, -0.14630, -0.10926, 0.12963],
+                                              [-0.07778, 0.03333,
+                                                  0.36667, -0.33333],
+                                              [-0.02901, -0.14630, -
+                                                  0.10926, 0.12963],
                                               [0.17778, 0.06667, -0.26667, 0.33333]]))
 
     def test_transforms(self):
@@ -192,20 +205,24 @@ class TupleTests(unittest.TestCase):
         transform = Matrix.scaling(2, 3, 4)
         self.assertEqual(m, m * transform * transform.inverse())
         v = Vector(-4, 6, 8)
-        self.assertEqual(v*transform, Vector(-8, 18,32))
+        self.assertEqual(v*transform, Vector(-8, 18, 32))
 
     def test_rotation(self):
         for axis in [0, 1, 2]:
             point = Point(1, 1, 1)
-            self.assertEqual(Matrix.rotating(math.pi, axis=axis)*Matrix.rotating(math.pi, axis=axis)*point, point)
+            print(Matrix.rotating(math.pi))
+            self.assertEqual(Matrix.rotating(math.pi, axis=axis)
+                             * Matrix.rotating(math.pi, axis=axis)*point, point)
 
     def test_shearing(self):
         start = [1, 0, 0, 0, 0, 0]
-        combos = [("x", "y"), ("x", "z"), ("y", "x"), ("y", "z"), ("z", "x"), ("z", "y")]
+        combos = [("x", "y"), ("x", "z"), ("y", "x"),
+                  ("y", "z"), ("z", "x"), ("z", "y")]
         while start[-1] == 0:
             p = Point(1, 2, 3)
             cur_comb = combos.pop(0)
-            setattr(p, cur_comb[0], getattr(p, cur_comb[0])+getattr(p, cur_comb[1]))
+            setattr(p, cur_comb[0], getattr(
+                p, cur_comb[0])+getattr(p, cur_comb[1]))
             self.assertEqual(Matrix.shearing(*start)*Point(1, 2, 3), p)
             start = [start[-1]] + start[:-1]
 
@@ -216,12 +233,12 @@ class TupleTests(unittest.TestCase):
         c = Canvas(40, 40)
         white = Color(1, 1, 1)
         while hands < 12:
-            c.write_pixel(center_point[0]+round(hand.x*(c.width*2/4)), center_point[1]+round(hand.y*(c.height*2/4)), white)
+            c.write_pixel(center_point[0]+round(hand.x*(c.width*2/4)),
+                          center_point[1]+round(hand.y*(c.height*2/4)), white)
             hand = (Matrix.rotating(math.pi/6, axis=2)*hand).normalize()
-            print(hand)
             hands = hands + 1
         c.ppm_file("out.ppm")
-        self.show_image()
+        # self.show_image()
 
     def test_ray_dist(self):
         r = Ray(Point(2, 3, 4), Vector(1, 0, 0))
@@ -229,3 +246,66 @@ class TupleTests(unittest.TestCase):
         self.assertEqual(r.position(1), Point(3, 3, 4))
         self.assertEqual(r.position(-1), Point(1, 3, 4))
         self.assertEqual(r.position(2.5), Point(4.5, 3, 4))
+
+    def test_ray_sphere_intersect(self):
+        r = Ray(Point(0, 0, -5), Vector(0, 0, 1))
+        s = Sphere(20)
+        self.assertEqual(s & r, (4.0, 6.0))
+        r = Ray(Point(0, 0, 5), Vector(0, 0, 1))
+        self.assertEqual(s & r, (-6.0, -4.0))
+        r = Ray(Point(0, 2, -5), Vector(0, 0, 1))
+        self.assertEqual(s & r, None)
+        r = Ray(Point(0, 1, -5), Vector(0, 0, 1))
+        self.assertEqual(s & r, (5.0, 5.0))
+        r = Ray(Point(0, 0, 0), Vector(0, 0, 1))
+        self.assertEqual(s & r, (-1.0, 1.0))
+
+    def test_hit(self):
+        s = Sphere(20)
+        i1 = Intersection(1, s)
+        i2 = Intersection(2, s)
+        self.assertEqual(Intersection.hit(i1 + i2), i1)
+        i1 = Intersection(-1, s)
+        i2 = Intersection(1, s)
+        self.assertEqual(Intersection.hit(i1 + i2), i2)
+        i1 = Intersection(-2, s)
+        i2 = Intersection(-1, s)
+        self.assertEqual(Intersection.hit(i1 + i2), None)
+        i1 = Intersection(5, s)
+        i2 = Intersection(7, s)
+        i3 = Intersection(-3, s)
+        i4 = Intersection(2, s)
+        self.assertEqual(Intersection.hit(i1 + i2 + i3 + i4), i4)
+
+    def test_ray_transforms(self):
+        r = Ray(Point(1, 2, 3), Vector(0, 1, 0))
+        m = Matrix.translating(3, 4, 5)
+        r2 = r.transform(m)
+        self.assertEqual(r2.origin, Point(4, 6, 8))
+        self.assertEqual(r2.direction, Vector(0, 1, 0))
+        r = Ray(Point(1, 2, 3), Vector(0, 1, 0))
+        m = Matrix.scaling(2, 3, 4)
+        r2 = r.transform(m)
+        self.assertEqual(r2.origin, Point(2, 6, 12))
+        self.assertEqual(r2.direction, Vector(0, 3, 0))
+
+    def test_transform_sphere_intersect(self):
+        r = Ray(Point(0, 0, -5), Vector(0, 0, 1))
+        s = Sphere(240, transform=Matrix.translating(5, 0, 0))
+        self.assertEqual(r & s, None)
+        s.transform = Matrix.scaling(2, 2, 2)
+        r = Ray(Point(0, 0, -5), Vector(0, 0, 1))
+        self.assertEqual(s & r, (3, 7))
+
+    @unittest.skipUnless(os.environ.get("TEST_RAY"), "this is too slow")
+    def test_raycast_sphere(self):
+        camera = Point(0, 0, -5)
+        background = (10, 7.0)
+        canvas_pixels = 200
+        pixel_size = background[1]/canvas_pixels
+        half = background[1]/2
+        canvas = Canvas(canvas_pixels, canvas_pixels)
+        color = Color(1, 0, 0)  # red
+        shape = Sphere(5)
+        [canvas.write_pixel(x, y, color) for x in range(canvas_pixels) for y in range(canvas_pixels) if shape & Ray(camera, (Point(-half + pixel_size * x, half - pixel_size * y, background[0]) - camera).normalize())]
+        canvas.ppm_file("out.ppm")
